e00d0c9c3fa6a29c7cc03c041794874782053b33
MostafaFouad0 <mm1471800@gmail.com>
2025-02-01 00:33:48 +0200

    added the try-catch block to the controllers

--- server/src/controllers/productsRevenues.js
+++ server/src/controllers/productsRevenues.js
@@ -1,36 +1,40 @@
 const dwhClient = require("../../prisma/dwh/client");
 
-const getProdcutsRevenues = async (req, res) => {
+const getProdcutsRevenues = async (req, res, next) => {
   const page = +req.query.page || 1;
   const limit = +req.query.limit || 10;
   const bussinessId = req.user.businessId;
-  const results = await dwhClient.productRevenueFact.findMany({
-    skip: (page - 1) * limit,
-    take: limit,
-    where: {
-      businessId: bussinessId,
-    },
-    orderBy: {
-      revenueAmount: "desc",
-    },
-    select: {
-      revenueAmount: true,
-      totalUnitsSold: true,
-      product: {
-        select: {
-          name: true,
+  try {
+    const results = await dwhClient.productRevenueFact.findMany({
+      skip: (page - 1) * limit,
+      take: limit,
+      where: {
+        businessId: bussinessId,
+      },
+      orderBy: {
+        revenueAmount: "desc",
+      },
+      select: {
+        revenueAmount: true,
+        totalUnitsSold: true,
+        product: {
+          select: {
+            name: true,
+          },
         },
       },
-    },
-  });
-  const formattedResponse = results.map((result) => ({
-    name: result.product.name,
-    revenueAmount: result.revenueAmount,
-    totalUnitsSold: result.totalUnitsSold,
-  }));
-  return res.status(200).json({
-    data: formattedResponse,
-  });
+    });
+    const formattedResponse = results.map((result) => ({
+      name: result.product.name,
+      revenueAmount: result.revenueAmount,
+      totalUnitsSold: result.totalUnitsSold,
+    }));
+    return res.status(200).json({
+      data: formattedResponse,
+    });
+  } catch (ex) {
+    next(ex);
+  }
 };
 
 module.exports = getProdcutsRevenues;
--- server/src/controllers/productsStock.js
+++ server/src/controllers/productsStock.js
@@ -1,33 +1,37 @@
 const dwhClient = require("../../prisma/dwh/client");
 const winston = require("winston");
 
-const getProductsStock = async (req, res) => {
+const getProductsStock = async (req, res, next) => {
   const page = +req.query.page || 1;
   const limit = +req.query.limit || 10;
   const bussinessId = req.user.businessId;
-  const results = await dwhClient.inventoryFact.findMany({
-    skip: (page - 1) * limit,
-    take: limit,
-    where: {
-      businessId: bussinessId,
-      currentStock: {
-        lte: 20,
-      },
-    },
-    include: {
-      product: {
-        select: {
-          name: true,
+  try {
+    const results = await dwhClient.inventoryFact.findMany({
+      skip: (page - 1) * limit,
+      take: limit,
+      where: {
+        businessId: bussinessId,
+        currentStock: {
+          lte: 20,
         },
       },
-    },
-    orderBy: {
-      currentStock: "asc",
-    },
-  });
-  return res.status(200).json({
-    data: results,
-  });
+      include: {
+        product: {
+          select: {
+            name: true,
+          },
+        },
+      },
+      orderBy: {
+        currentStock: "asc",
+      },
+    });
+    return res.status(200).json({
+      data: results,
+    });
+  } catch (ex) {
+    next(ex);
+  }
 };
 
 module.exports = getProductsStock;
