d56231741b2a521626e5f12c8bd681aa880b46da
Mohab <mohabyaserofficial2003@gmail.com>
2025-01-31 21:11:17 +0200

    Refactor category revenue route to use user authentication and authorization

--- server/src/routes/dashboard.js
+++ server/src/routes/dashboard.js
@@ -1,6 +1,12 @@
 const dashboardRouter = require("express").Router();
 const { categoryRevenueController } = require("../controllers/categoryRevenue");
+const authenticate = require("../middlewares/authenticate");
+const authorize = require("../middlewares/authorizate");
 
-dashboardRouter.get("/category-revenue/:businessId", categoryRevenueController);
+dashboardRouter.get(
+  "/category-revenue",
+  [authenticate, authorize],
+  categoryRevenueController
+);
 
 module.exports = dashboardRouter;
