2a4f8827b69b34aa140cff826b333ee1968ad1a1
MostafaFouad0 <mm1471800@gmail.com>
2025-02-03 22:57:16 +0200

    changed endpoint name

--- server/src/utils/endpoints.js
+++ server/src/utils/endpoints.js
@@ -3,8 +3,8 @@ const AUTH = `${VERSION}/auth`;
 const STAFF = `${VERSION}/staff`;
 const ADMIN = `${VERSION}/admin`;
 const DASHBOARD = `${VERSION}/dashboard`;
-const PRODUCTS_REVENUES = `${VERSION}/products-revenues`;
-const PRODUCTS_STOCK = `${VERSION}/products-stock`;
+const PRODUCTS_REVENUES = `${VERSION}/statistics/products-revenues`;
+const PRODUCTS_STOCK = `${VERSION}/statistics/products-stock`;
 module.exports = {
   AUTH,
   STAFF,
