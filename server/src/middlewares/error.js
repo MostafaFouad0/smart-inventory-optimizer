fa816ee84d857b9539562946152cb821fea9b8ff
Youssif Ahmed <joahmed002@gmail.com>
2025-02-04 23:11:07 +0200

    [200~fix: prevent crashing on headers already sent in error middleware

--- server/src/middlewares/error.js
+++ server/src/middlewares/error.js
@@ -2,5 +2,6 @@ const winston = require("winston");
 
 module.exports = function (err, req, res, next) {
   winston.error(err);
+  if (res.headersSent) return next(err);
   res.status(500).json({ message: "internal server error" });
 };
