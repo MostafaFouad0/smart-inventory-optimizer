336454be59756f9bbbcb79a27e3a2f6c07450de1
MostafaFouad0 <mm1471800@gmail.com>
2025-02-05 01:40:51 +0200

    feat: add Swagger UI integration and API documentation

--- server/src/startup/routes.js
+++ server/src/startup/routes.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
+const YAML = require("yamljs");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = YAML.load(path.join(__dirname, "../../swagger.yaml"));
 const endpoints = require("../utils/endpoints");
 const error = require("../middlewares/error");
 const staff = require("../routes/staff");
@@ -9,6 +13,11 @@ const statisticsRouter = require("../routes/statistics");
 module.exports = function (app) {
   app.use(cors());
   app.use(express.json());
+  app.use(
+    endpoints.API_DOCS,
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument)
+  ); // for swagger documentation
   app.use(endpoints.STAFF, staff);
   app.use(endpoints.AUTH, auth);
   app.use(endpoints.STATISTICS, statisticsRouter);
