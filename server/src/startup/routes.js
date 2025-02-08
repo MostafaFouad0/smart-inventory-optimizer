bca51383a52c49d0f85883f117a25e42fa69b568
MostafaFouad0 <mm1471800@gmail.com>
2025-02-08 13:09:11 +0200

    fix: downgrade swagger-ui-express to version 4.3.0 and update custom CSS integration to fix a depolyment issue

--- server/src/startup/routes.js
+++ server/src/startup/routes.js
@@ -9,14 +9,19 @@ const error = require("../middlewares/error");
 const staff = require("../routes/staff");
 const auth = require("../routes/auth");
 const statisticsRouter = require("../routes/statistics");
-
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 module.exports = function (app) {
   app.use(cors());
   app.use(express.json());
   app.use(
     endpoints.API_DOCS,
     swaggerUi.serve,
-    swaggerUi.setup(swaggerDocument)
+    swaggerUi.setup(swaggerDocument, {
+      customCss:
+        ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
+      customCssUrl: CSS_URL,
+    })
   ); // for swagger documentation
   app.use(endpoints.STAFF, staff);
   app.use(endpoints.AUTH, auth);
