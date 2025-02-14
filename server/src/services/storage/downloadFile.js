54dbd932b013f91b0ac59ebc736718d832af83a8
Youssif Ahmed <joahmed002@gmail.com>
2025-02-14 19:11:01 +0200

    fix: fixed the downloadfile module

--- server/src/services/storage/downloadFile.js
+++ server/src/services/storage/downloadFile.js
@@ -1,4 +1,7 @@
+const client = require("./bucketClient");
+
 async function downloadFile(path, bucketName) {
   const { data, error } = await client.storage.from(bucketName).download(path);
+  return { data, error };
 }
 module.exports = downloadFile;
