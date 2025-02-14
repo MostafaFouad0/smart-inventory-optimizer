9963e585f1459b5cb828259c2a3721df61f528d7
MostafaFouad0 <mm1471800@gmail.com>
2025-02-14 16:49:45 +0200

    feat: add CSV parser utility function

---
+++ server/src/utils/CSVParser.js
@@ -0,0 +1,13 @@
+const client = require("./bucketClient");
+const { Readable } = require("stream");
+
+const readCSV = async (bucketName, path) => {
+  const { data, error } = await client.storage.from(bucketName).download(path);
+  if (error) throw error;
+
+  const csvText = await data.text();
+  const readableStream = Readable.from(csvText);
+  return readableStream;
+};
+
+module.exports = readCSV;
