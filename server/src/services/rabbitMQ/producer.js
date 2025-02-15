19d9517b7b05ae0bfb4cb94b52fbec2d2a460b3a
MostafaFouad0 <mm1471800@gmail.com>
2025-02-15 21:42:53 +0200

    feat: implement purchases consumer and enhance message handling with status updates

--- server/src/services/rabbitMQ/producer.js
+++ server/src/services/rabbitMQ/producer.js
@@ -1,18 +1,38 @@
 const winston = require("winston");
 const { createChannel } = require("./rabbitMQ");
+const client = require("../../../prisma/main/client");
 
 const sendMessage = async (queue, message) => {
+  let channel;
   try {
-    const channel = await createChannel();
-
+    channel = await createChannel();
+    const data = await client.cSVStatus.create({
+      data: {
+        businessId: message.businessId,
+        uploadedDate: message.uploadedDate,
+        status: "In queue",
+        path: message.fileName,
+      },
+    });
+    message.id = data.id;
     await channel.assertQueue(queue, { durable: true });
-    channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
-    await channel.close();
+    await channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
+
     if (process.env.NODE_ENV === "development") {
       winston.debug(`Sent message to ${queue}:`, message);
     }
   } catch (error) {
-    winston.error("Error sending message to RabbitMQ:", error);
+    /// delete the record from the database if the message could not be sent
+    if (message.id) {
+      await client.cSVStatus.delete({
+        where: {
+          id: message.id,
+        },
+      });
+    }
+    return error;
+  } finally {
+    if (channel) await channel.close();
   }
 };
 
---
+++ server/src/services/rabbitMQ/purchasesConsumer.js
@@ -0,0 +1,52 @@
+const { createChannel } = require("./rabbitMQ");
+const readCSV = require("../../utils/CSVParser");
+const validatePurchases = require("../../validators/purchasesCSValidator");
+const insertBatchTransactions = require("../maindb/insertBatchTransactions");
+const client = require("../../../prisma/main/client");
+const changeStatus = require("../maindb/changeStatus");
+const winston = require("winston");
+
+const purchasesConsumer = async () => {
+  let channel;
+  try {
+    channel = await createChannel();
+    const queue = "purchases";
+
+    await channel.assertQueue(queue, { durable: true });
+    winston.info("Purchases consumer started successfully...");
+    await channel.consume(
+      queue,
+      async (msg) => {
+        if (msg !== null) {
+          try {
+            const message = JSON.parse(msg);
+            changeStatus(message.id, "Processing");
+            const stream = await readCSV(message.bucketName, message.fileName);
+            const data = await validatePurchases(stream, {
+              businessId: message.businessId,
+            });
+            if (data.badRows.length > 0) {
+              changeStatus(message.id, "Failed");
+              channel.ack(msg);
+            } else {
+              await insertBatchTransactions(data, message);
+              changeStatus(message.id, "Done");
+            }
+            channel.ack(msg);
+          } catch (error) {
+            console.error("Error processing message:", error);
+            changeStatus(message.id, "Failed");
+            channel.nack(msg, false, false);
+          }
+        }
+      },
+      {
+        noAck: false,
+      }
+    );
+  } catch (error) {
+    console.error("Error in purchases consumer:", error);
+  }
+};
+
+module.exports = purchasesConsumer;
