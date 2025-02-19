8f4e40d77aba6356b594269019739bcab410eeb3
Mostafa Mahmoud Fouad <mm1471800@gmail.com>
2025-02-19 21:28:36 +0200

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -8,8 +8,8 @@ async function run() {
   await require("./startup/db")();
   await require("./startup/routes")(app);
   await require("./services/rabbitMQ/rabbitMQ").getConnection();
-  await require("./services/rabbitMQ/purchasesConsumer")();
-  await require("./services/rabbitMQ/salesConsumer")();
+  // await require("./services/rabbitMQ/purchasesConsumer")();
+  // await require("./services/rabbitMQ/salesConsumer")();
   const port = process.env.PORT || 3000;
   app.listen(port, () => winston.info(`Listening on port ${port}...`));
 }
